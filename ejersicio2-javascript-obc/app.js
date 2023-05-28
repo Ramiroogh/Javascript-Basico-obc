f8bfa53bf1c375383e5f84ab6190992fcfd04472
ramiro <navarrete01@gmail.com>
2023-05-28 16:13:20 -0300

    ejercicio2

---
+++ ejersicio2-javascript-obc/app.js
@@ -0,0 +1,11 @@
+let nombre = 'ramiro';
+let edad = 22;
+let desarrollador = true;
+let nacimiento = new Date(2001, 3, 27)
+
+let libroFavorito = {
+    titulo: 'clean code',
+    autor: 'robert c. martin',
+    fecha: new Date(2008, 5, 11),
+    url: 'https://www.amazon.com/-/es/Robert-C-Martin/dp/0132350882',
+}
